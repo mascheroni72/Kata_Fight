50fec7d1c830b25e126cb80f55e07e45ee3a9ac3
Pablo Tulian <agustinmascheroni72@gmail.com>
2024-04-01 23:53:41 +0200

    Variación fuerza

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once "luchador.php";
 require_once "combate.php";
 
-$luchador1 = new Luchador ("Martín Karadagián", 10, 8, 6);
+$luchador1 = new Luchador ("Martín Karadagián", 10, 6, 6);
 $luchador2 = new Luchador ("La Momia", 10, 8, 6);
 
 $combate = new Combate ($luchador1, $luchador2);
